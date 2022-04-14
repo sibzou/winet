ddc2286bd75e65e2a7c854520b56334d5fdc6729
kevinBoudinar <kevin.boudina6@etu.univ-lorraine.fr>
2022-04-14 15:56:27 +0200

    final topbar font

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import "./topbar.css"
 import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 export default function Topbar(){
     return(
@@ -11,11 +14,31 @@ export default function Topbar(){
 
             <div className="topbarCenter">
                 <div className="searchbar">
-                    <SearchIcon />
-                    <input type="text" placeholder="Search" />
+                    <SearchIcon className="searchIcon"/>
+                    <input type="text" placeholder="Search for a wine" className="searchInput" />
                 </div>
             </div>
-            <div className="topbarRight"></div>
+            <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">Homepage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <PersonIcon />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <ChatIcon />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <NotificationsIcon />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+                <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
+            </div>
 
         </div>
     )
