f25906b32ca12c88a9a85a6bfe30f8c6378d032d
kevinBoudinar <kevin.boudina6@etu.univ-lorraine.fr>
2022-04-18 15:41:18 +0200

    Sidebar component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,9 +1,13 @@
 import "./feed.css";
+import Share from "../share/Share";
+
 
 export default function Feed(){
     return (
         <div className="feed">
-            feed
+            <div className="feedWrapper">
+                <Share/>
+            </div>
         </div>
 
     )
---
+++ src/components/share/Share.jsx
@@ -0,0 +1,9 @@
+import "./share.css";
+
+export default function Share(){
+    return (
+        <div>
+            its share
+        </div>
+    )
+}
