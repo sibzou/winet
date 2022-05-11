10616d6d2e539bfe9de620d48680c292782d7081
sibzou <91130460+sibzou@users.noreply.github.com>
2022-05-11 12:45:51 +0200

    Implement the server registration

--- back/src/public/index.php
+++ back/src/public/index.php
@@ -1,10 +1,30 @@
 <?php
+    $method = $_SERVER["REQUEST_METHOD"];
     $uri = $_SERVER["REQUEST_URI"];
 
-    if($uri == "/signup") {
-        echo "Not implemented";
+    $inputJson = file_get_contents("php://input");
+    $input = json_decode($inputJson);
+
+    $db = new SQLite3("../../db");
+
+    function executeSql($stmt) {
+        if($stmt->execute()) {
+            http_response_code(200);
+        } else {
+            http_response_code(500);
+        }
+    }
+
+    if($method == "POST" && $uri == "/signup") {
+        $stmt = $db->prepare("insert into user(name, password) values(:username, :password)");
+        $passwordHash = password_hash($input->password, PASSWORD_DEFAULT);
+
+        $stmt->bindValue("username", $input->username);
+        $stmt->bindValue("password", $passwordHash);
+
+        executeSql($stmt);
     } else {
         http_response_code(404);
-        echo "Not found";
+        echo "Not found\n";
     }
 ?>
