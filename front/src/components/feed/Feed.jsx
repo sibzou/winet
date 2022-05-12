21b7a1278cf7ffd8f76fd4ca36aced09f3e8425d
sibzou <91130460+sibzou@users.noreply.github.com>
2022-05-12 17:21:04 +0200

    Add favorite button and rate buttons

--- front/src/components/feed/Feed.jsx
+++ front/src/components/feed/Feed.jsx
@@ -5,6 +5,7 @@ import {useState} from "react";
 
 export default function Feed(props){
     const searchResultsNodes = [];
+    const [currentWineId, setCurrentWineId] = useState();
     const [currentWine, setCurrentWine] = useState(null);
 
     const onWineDetails = event => {
@@ -24,6 +25,32 @@ export default function Feed(props){
         };
 
         xhr.send(JSON.stringify(details));
+        setCurrentWineId(wineId);
+    }
+
+    const sendRate = rate => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:8080/rate");
+
+        const rateReq = {
+            token: props.token,
+            wineId: currentWineId,
+            rate: rate
+        }
+
+        xhr.send(JSON.stringify(rateReq));
+    }
+
+    const favoriteWine = () => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:8080/favorite");
+
+        const favoriteReq = {
+            token: props.token,
+            id: currentWineId
+        }
+
+        xhr.send(JSON.stringify(favoriteReq));
     }
 
     if(props.searchResults) {
@@ -37,12 +64,26 @@ export default function Feed(props){
             </div>
         )
     } else if(currentWine != null) {
+        const rateNodes = [];
+
+        for(let i = 0; i <= 10; i++) {
+            rateNodes.push(<p onClick={() => sendRate(i)}>{i}</p>);
+        }
+
         return (
             <div>
                 <p>{currentWine.name}</p>
                 <p>{currentWine.category}</p>
                 <p>{currentWine.vineyard}</p>
                 <p>{currentWine.color}</p>
+                {currentWine.rate &&
+                <p>{currentWine.rate}/10</p>
+                }
+                <p>Notez le vin :</p>
+                <div className="rate-buttons">
+                    {rateNodes}
+                </div>
+                <div className="favorite-button" onClick={() => favoriteWine()}>Ajouter aux coups de cœur</div>
             </div>
         )
     }
